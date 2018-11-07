59b5c33f400dcd79fefc21a4f0c7ac1969c97d0a
Israq <35467653+Israq@users.noreply.github.com>
2018-11-07 08:54:15 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int a;
+ int b;
+ int sum;
+
+ cout<<"Enter the firstnumber to sum\n";
+ cin>>a;
+
+ cout<<"Enter the second number to sum\n";
+ cin>>b;
+
+ sum=a+b;
+ cout<<"The sum is   "<<sum<<endl;
+ return 0;
+
+}
